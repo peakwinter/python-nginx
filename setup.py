020109f88c4b65e154546f2a3cf4ea8bad961400
jacook <jacob@jcook.cc>
2013-11-24 16:47:21 -0500

    make as python module

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='python-nginx',
+    version='0.1',
+    description='Create and modify nginx serverblock configs in Python',
+    author='Jacob Cook',
+    author_email='jacob@jcook.cc',
+    url='https://jcook.cc',
+    packages=['nginx']
+)
