406612f9ee374233641574746c6d8c8877aaf0c8
fulder <misad90@gmail.com>
2018-03-30 15:51:08 +0200

    Add test from PR #21 and issue #20

--- tests.py
+++ tests.py
@@ -102,6 +102,43 @@ location /{ test_key test_value; }}
 """
 
 
+TESTBLOCK_CASE_5 = """
+upstream test0 {
+    server 1.1.1.1:8080;
+    send "some request";
+}
+
+upstream test1 {
+    server 1.1.1.1:8080;
+    send 'some request';
+}
+
+server {
+    server_name "www.example.com";
+
+    location / {
+        root html;
+    }
+}
+"""
+
+
+TESTBLOCK_CASE_6 = """
+upstream test0 {
+    server 1.1.1.1:8080;
+    check interval=3000 rise=2 fall=3 timeout=3000 type=http;
+    check_http_send "GET /alive.html  HTTP/1.0\r\n\r\n";
+    check_http_expect_alive http_2xx http_3xx;
+}
+
+upstream test1 {
+    ip_hash;
+    server 2.2.2.2:9000;
+    check_http_send 'GET /alive.html  HTTP/1.0\r\n\r\n';
+}
+"""
+
+
 class TestPythonNginx(unittest.TestCase):
     def test_basic_load(self):
         self.assertTrue(nginx.loads(TESTBLOCK_CASE_1) is not None)
@@ -165,6 +202,16 @@ class TestPythonNginx(unittest.TestCase):
         inp_data = nginx.loads(TESTBLOCK_CASE_1)
         out_data = '\n' + nginx.dumps(inp_data)
         self.assertEqual(TESTBLOCK_CASE_1, out_data)
+
+    def test_quoted_key_value(self):
+        data = nginx.loads(TESTBLOCK_CASE_5)
+        out_data = '\n' + nginx.dumps(data)
+        self.assertEqual(out_data, TESTBLOCK_CASE_5)
+
+    def test_complex_upstream(self):
+        inp_data = nginx.loads(TESTBLOCK_CASE_6)
+        out_data = '\n' + nginx.dumps(inp_data)
+        self.assertEqual(TESTBLOCK_CASE_6, out_data)
 
     def test_filtering(self):
         data = nginx.loads(TESTBLOCK_CASE_1)
