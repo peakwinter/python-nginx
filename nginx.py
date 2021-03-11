ae470805c482b94b3eb96c5b46264169eca1e61b
Jacob Cook <jacob@peakwinter.net>
2021-03-10 22:48:27 -0500

    fix: bug with key value being an integer

--- nginx.py
+++ nginx.py
@@ -399,7 +399,7 @@ class Key(object):
         """Return key as nginx config string."""
         if self.value == '' or self.value is None:
             return '{0};\n'.format(self.name)
-        if '"' not in self.value and (';' in self.value or '#' in self.value):
+        if type(self.value) == str and '"' not in self.value and (';' in self.value or '#' in self.value):
             return '{0} "{1}";\n'.format(self.name, self.value)
         return '{0} {1};\n'.format(self.name, self.value)
 
